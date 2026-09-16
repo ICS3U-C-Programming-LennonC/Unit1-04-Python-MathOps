72b26b9469adc4d1ed18da297533fad181502f5b
Lennon Cehajic <lennon.cehajic@stu.ocsb.ca>
2026-09-16 13:20:30 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Created By: Lennon Cehajic
+# Date: September 16th, 2026
+# This program runs basic math.
+
+
+def main():
+    print("2+3={}".format(2 + 3))
+    print("5-4={}".format(5 - 4))
+    print("2*3={}".format(2 * 3))
+    print("55/12={}".format(55 / 12))
+    print("5%2={}".format(5 % 2))
+    print("5**4={}".format(5**4))
+    print("25**0.5={}".format(25**0.5))
+
+
+if __name__ == "__main__":
+    main()
